ea0064bc1b4d3a8fa5aa259b19deb9e55492ae8c
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-14 16:02:59 -0600

    update

--- WiseCredit/ContentView.swift
+++ WiseCredit/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "doc.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! Kike")
         }
         .padding()
     }
