72e7bd4ebac1eb343f96b2af32f867a8b8b58080
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-14 15:55:38 -0600

    Initial Commit

---
+++ WiseCredit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WiseCredit
+//
+//  Created by Enrique Macias on 9/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WiseCredit/WiseCreditApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseCreditApp.swift
+//  WiseCredit
+//
+//  Created by Enrique Macias on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WiseCreditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
