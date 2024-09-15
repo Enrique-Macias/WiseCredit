6dc7acd7248664d0a26f73c0f513de95a9262202
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-15 03:19:27 -0600

    Credit Simulation Implementation

---
+++ WiseCredit/CreditSimulationView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditSimulationView.swift
+//  WiseCredit
+//
+//  Created by Enrique Macias on 9/15/24.
+//
+
+import SwiftUI
+
+struct CreditSimulationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditSimulationView()
+}
